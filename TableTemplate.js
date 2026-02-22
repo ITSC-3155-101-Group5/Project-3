20ca8f022916babca4be380b73fc0229415d760e
jlienghot <jlienghot@outlook.com>
2026-02-22 00:59:30 -0500

    Header Row Processing

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,23 +1,39 @@
 "use strict";
 
-function TemplateProcessor(template) {
+function TableTemplate(template) {
   this.template = template;
 }
 
-// Replace placehodler in the template with values from the data object 
-TemplateProcessor.prototype.fillIn = function (data) {
-  // use regex to find all placeholder of the form (property)
-  // the (.*?) catch the property name inside braces 
-  return this.template.replace(/{{(.*?)}}/g, function(match, property) {
+TableTemplate.fillIn = function (tableId, dictionary, columnName) {
 
-    // check if data object cointain the request property
-    if (data.hasOwnProperty(property)) {
-      // replace holder with the matching value 
-      return data[property];
+    const table = document.getElementById(tableId);
+
+    if (!table) {
+        return;
     }
 
-    // if the  property does not natch , replace with an empty string   
-    return "";
-  });
+    const rows = table.rows;
+
+    if (!rows || rows.length === 0) {
+        return;
+    }
+
+    const headerRow = rows[0];
+    const headerCells = headerRow.cells;
+
+    for (let i = 0; i < headerCells.length; i++) {
+        const cell = headerCells[i];
+        let text = cell.innerHTML;
+
+        text = text.replace(/{{(.*?)}}/g, function(match, property) {
+            property = property.trim();
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                return dictionary[property];
+            }
+            return "";
+        });
+
+        cell.innerHTML = text;
+    }
 
 };
